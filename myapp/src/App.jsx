0fe7b56a90de44e1294a55416d9dcf9d80c2c411
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-03-05 15:25:58 +0530

    Update App.jsx

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -16,7 +16,6 @@ import Todos from "./components/01-03/Todos";
 import UseMemo from "./components/01-03/UseMemo";
 import UseCallBack from "./components/01-03/UseCallBack";
 import UseRef from "./components/02-03/UseRef";
-import UseRef from "./components/02-03/UseReducer";
 import UseReducer from "./components/02-03/UseReducer";
 function App() {
   const [counter, setCounter]=useState(0);
@@ -41,7 +40,6 @@ function App() {
       <Route path="/usememo" element={<UseMemo />} />
       <Route path="/use-call-back" element={<UseCallBack />} />
       <Route path="/useref" element={<UseRef />} />
-      <Route path="/usereducer" element={<UseReducer />} /
       <Route path="/usereducer" element={<UseReducer />} />
       </Routes>
     </div>
