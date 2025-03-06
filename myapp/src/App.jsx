aaa7dbea5fe2e730e239d378afe7587bed54d3b0
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-03-06 20:43:58 +0530

    update

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -11,27 +11,19 @@ import Home from "./components/Home";
 import Props from "./components/23-02/Props";
 import { useContext, useState } from "react";
 import Todo from "./components/23-02/Todo";
-<<<<<<< HEAD:myapp/src/App.js
-import MyTodo from "./components/01-03/MyTodo";
-
-
-=======
 import Register1 from "./components/28-02/Register1";
 import Todos from "./components/01-03/Todos";
 import UseMemo from "./components/01-03/UseMemo";
 import UseCallBack from "./components/01-03/UseCallBack";
 import UseRef from "./components/02-03/UseRef";
-<<<<<<< HEAD
->>>>>>> 5c335eb14911153e17b6c6c46355a8b815dc55ad:myapp/src/App.jsx
-=======
 import UseReducer from "./components/02-03/UseReducer";
 import { NameCounterContext } from "./context/CounterContext";
 import ContextCounter from "./components/04-03/ContextCounter";
->>>>>>> 7f642267a1ea1ce2a0bcb225ac7d9d0241159b03
+
 function App() {
   const [counter, setCounter]=useState(0);
   const {state, dispatch}=useContext(NameCounterContext)
-  console.log(state, "state from the app")
+  console.log(state, "state from the app", dispatch,"dispatch from the app" )
 
   console.log("Inside app");
   return (
@@ -47,25 +39,15 @@ function App() {
         <Route path="/useparams"element={<UseParams />} />
         <Route path="/paramsproduct/:uniqueid"element={<ParamsProduct />} />
         <Route path="/props" element={<Props counter={counter} setCounter={setCounter} />} />
-<<<<<<< HEAD:myapp/src/App.js
         <Route path="/todo" element={<Todo />} />
-        <Route path="/mytodo" element={<MyTodo />} />
-  
-   
-=======
         <Route path="/register1"element={<Register1/>}/>
-
-      <Route path="/todo" element={<Todo />} />
+        <Route path="/todo" element={<Todo />} />
       <Route path="/todos" element={<Todos />} />
       <Route path="/usememo" element={<UseMemo />} />
       <Route path="/use-call-back" element={<UseCallBack />} />
       <Route path="/useref" element={<UseRef />} />
-<<<<<<< HEAD
->>>>>>> 5c335eb14911153e17b6c6c46355a8b815dc55ad:myapp/src/App.jsx
-=======
       <Route path="/usereducer" element={<UseReducer />} />
       <Route path="/context-counter" element={<ContextCounter />} />
->>>>>>> 7f642267a1ea1ce2a0bcb225ac7d9d0241159b03
       </Routes>
     </div>
   );
