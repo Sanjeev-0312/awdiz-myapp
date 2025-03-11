7049ec70d6142d617a251d89de7d1915e6340681
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-03-11 17:42:04 +0530

    update

--- myapp/src/components/08-03/AllProducts.jsx
+++ myapp/src/components/08-03/AllProducts.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
 const AllProducts = () => {  
+    const [allProducts, setAllProducts] = useState([]);
+    const router = useNavigate();
+    const token= useSelector((state) => state.user.token);
+
+    const getAllProducts = async() =>{
+        try{
+            const response =await axions.get("https://fakestoresapi.com/products");
+            console.log(response.data, "response");
+            setAllProducts(response.data);
+        }catch(error){
+            console.log(error);
+        }
+    };
+    useEffect (() => {
+        if(token == null){
+            router ("/fake-login");
+        }
+        getAllProducts();
+    },[token]);
     return (
-        <div>All Products</div>
+        <div>All Products
+          <div style={{
+            display:"flex",
+            justifyContent:"space-around",
+            flexWrap:"wrap",
+          }} >
+
+          </div>
+        </div>
     );
 };
 
