30e20e22b50c46a07ee14e0bd2577b61ee7b2993
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-03-12 23:14:32 +0530

    update

--- myapp/src/redux/store.js
+++ myapp/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice  from './slice/counterSlice'
 import themeSlice from './slice/themeSlice';
+import userReducer from "./userSlice.js";
 
 export const store = configureStore({
   reducer: {
     counter : counterSlice,
     theme: themeSlice,
+    user: userReducer,
   },
 });
