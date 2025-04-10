bdbcd21c918cf5a96b1e198f563653c4b308a621
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-04-10 18:34:55 +0530

    update

--- myapp/src/components/28-02/Register1.jsx
+++ myapp/src/components/28-02/Register1.jsx
@@ -1,91 +1,4 @@
-// import {useState} from "react";
-// import { useNavigate } from "react-router-dom";
-// function Register1(){
-//     const router= useNavigate();
-//     const[allUsers, setAllUsers]= useState([]);
-//     console.log(allUsers, "allUsers");
-//     cosnt [userData, setUsersData] = useState({
-//         name: "",
-//         email: "",
-//         password:"",
-//         confirmPassword:"",
-//     });
-//     console.log(userData,"userData");
-//     const handleChange =(event) =>{
-//         setUsersData({...userData, [event.target.name]: event.target.value});
-//     };
-//     const handleSubmit = async (event)=>{
-//         event.preventDefault();
-//         if(
-//             userData.name &&
-//             userData.email &&
-//             userData.password &&
-//             userData.confirmPassword 
-//         ){
-//             if(userData.password===userData.confirmPassword){
-//                 setAllUsers([...allUsers,userData]);
-//                 setUsersData({
-//                     name:"",
-//                     email:"",
-//                     password:"",
-//                     confirmPassword:"",
-//                 });
-//             }else{
-//                 alert("Password not Matched.")
-//             }
-//         }else{
-//             alert("All fields are required.");
-//       };
-     
-//     }
-//     return (
-//         <div>
-//             <h1>Register</h1>
-//             <button onClick={() => router("/login")}>Login</button>
-//             <button onClick={() => router("/")}>Home</button>
-
-//             <form onSubmit={handleSubmit}>
-//                 <label>Name:</label>
-//                 <br />
-//                 <input type="text" value={userData.name} onChange={handleChange} name="name" />
-
-//                 <br />
-
-//                 <label >Email: </label>
-//                 <br />
-//                 <input type="email" value={userData.email} onChange={handleChange} name="email"/>
-
-//             <br />
-           
-//            <label>Password:</label>
-//            <br />
-//            <input type="password" value={userData.password} onChange={handleChange} name="password" />
-
-//            <br />
-
-//            <label>Confirm Password:</label>
-//            <br />
-//            <input type="password" value={userData.confirmPassword}
-//            onChange={handleChange} name="confirmPassword" />
-//            <br />
-//            <input type="submit" />
-//               <br />
-//             </form>
-//             <h1>All Users</h1>
-//       {/* {user.email } */}
-//       {allUsers.map((user, i) => (
-//         <div>
-//           <h1>{i + 1}</h1>
-//           <h1>Name : {user.name}</h1>
-//           <h3>Email : {user.email}</h3>
-//         </div>
-//       ))};
-//     </div>
-//        );
-// }
-// export default Register1;
-
-
+import axios from "axios";
 import { useState } from "react";
 import React from "react-router-dom";  //i can use here from useNavigate 
 
@@ -111,6 +24,9 @@ function Register1() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+     try{
+
     if (
       userData.name &&
       userData.email &&
@@ -118,6 +34,15 @@ function Register1() {
       userData.confirmPassword
     ) {
       if (userData.password === userData.confirmPassword) {
+
+        const response = await axios.post(
+          "http://localhost:8000/api/v1/auth/register",
+          {
+            userData,
+          }
+        );
+        console.log(response,"response");
+        
         setAllUsers([...allUsers, userData]);
         setUserData({
           name: "",
@@ -131,6 +56,9 @@ function Register1() {
     } else {
       alert("All fields are required.");
     } 
+    }catch(error){
+      console.log(error,"error while submiting register.");
+    }
   };
 
   return (
