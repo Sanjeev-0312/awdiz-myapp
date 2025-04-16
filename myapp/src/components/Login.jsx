99acc4774e94b94f5049ade4f560552d519aee0f
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-04-16 15:10:48 +0530

    me

--- myapp/src/components/Login.jsx
+++ myapp/src/components/Login.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 function Login() {
+  const [userData, setUserData]= useState({email:"", password : ""});
   const router = useNavigate();
+  const handleChange = (e) => {
+    setUserData({...userData, [e.target.name]: e.target.value});
+  };
+
   return (
     <div>
       <h1 className="head">Login</h1>
@@ -12,11 +18,11 @@ function Login() {
           Email : 
         </label>
         <br />
-        <input type="email" />
+        <input type="email" name="email" />
         <br />
         <label>Password :</label>
         <br />
-        <input type="password" />
+        <input type="password" name="password" />
         <br />
         <input type="submit" />
         <br />
