d3aa9d12f563e646a4002126b9f844110757e460
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-05-07 13:38:17 +0530

    socket.io

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -6,10 +6,10 @@ import UseState from "./components/22-02/UseState";
 import UseEffects from "./components/22-02/UseEffects";
 import UseParams from  "./components/22-02/UseParams";
 import ParamsProduct from "./components/22-02/ParamsProduct";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, data } from "react-router-dom";
 import Home from "./components/Home";
 import Props from "./components/23-02/Props";
-import { useContext, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import Todo from "./components/23-02/Todo";
 import Register1 from "./components/28-02/Register1";
 // import Todos from "./components/01-03/Todos";
@@ -26,6 +26,12 @@ import SingleProduct from "./components/08-03/SingleProduct";
 import NavBar from "./components/08-03/NavBar";
 import { login } from "./redux/slice/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { io } from "socket.io-client";
+
+
+const socket = io("http://localhost:8000", {
+  withCredentials: true
+});
   
 function App() {
   const dispatch = useDispatch();
@@ -42,10 +48,28 @@ function App() {
         dispatch(login(tokenFromLocalStorage));
       }
     }
+   
+
   }, []);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data, "received message");
+    });
+  
+    return () => {
+      socket.off("receive_message"); // Clean up listener on unmount
+    };
+  }, []);
+
+  console.log("data",data)
+  const sendMessage = () =>{
+    socket.emit("send_message", {message: "Hello from frontend"});
+  };
   return (
     <div className="nav"><NavBar/>
     <div className="App">
+    <button onClick={sendMessage}>Socket check</button>
     
       <Routes>
       
